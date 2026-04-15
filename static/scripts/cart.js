2a12fd919a88618c0a42be45be5e4a6672c3e287
IGamePeI <rahmatulinamir5@gmail.com>
2026-04-15 14:33:37 +0000

    byg fix

---
+++ static/scripts/cart.js
@@ -0,0 +1,16 @@
+container
+let cart = localStorage.getItem("cart");
+if(!cart) {
+    cart = [];
+}
+
+container = document.getElementById();
+
+function createDishCard(dish) {
+}
+
+document.addEventListener('DOMContentLoaded', async() => {
+    cart.forEach(dish_element => {
+        createDishCard(dish_element);
+    });
+});
