302ee876fe06b1abce2e140c08cb4a0510be810e
IGamePeI <rahmatulinamir5@gmail.com>
2026-04-16 12:03:26 +0000

    main logic and base cart

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,17 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const authForm = document.getElementById('auth-form');
-    authForm.addEventListener('submit', async () => {
+async function createLogin() {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         response = await fetch('http://127.0.0.1:8000/auth/login', {
             method: 'POST',
             credentials: 'include',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({email, password}),
         }).then(data => {
-            window.location.href = '/'
+            console.log(data);
+            window.location.href = '/';
         }).catch(error => {
             console.error(error);
         });
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const authForm = document.getElementById('auth_form');
+    authForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        await createLogin();
     });
 });
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -6,12 +6,6 @@ if(!cart) {
     cart = JSON.parse(cart);
 }
 
-
-
-let addButton = document.getElementById('add_to_cart');
-
-
-
 function createDishCard(dish) {
     const card = document.createElement('div');
     card.className = 'dish';
@@ -23,12 +17,12 @@ function createDishCard(dish) {
         <span id="dish_name" class="dish_name" dish_id="${dish.id}">${dish.name}</span>
         <span id="dish_weight" class="dish_weight">${dish.weight} г</span>
         <span id="dish_price" class="dish_price">${dish.price} руб</span>
-        <button class="add_to_cart" id="add_to_cart">Добавить в корзину</button>
+        <button class="add_to_cart" id="add_to_cart_${dish.id}">Добавить в корзину</button>
     `;
-
-
-    return card;
+    
+    return [card, dish.id];
 }
+
 
 
 function renderMenus(dishes) {
@@ -42,16 +36,19 @@ function renderMenus(dishes) {
         } else {
             container = document.getElementById('future_menu');
         }
-        container.appendChild(card);
+        container.appendChild(card[0]);
+        let button = document.getElementById(`add_to_cart_${card[1]}`);
+        button.addEventListener('click', addToCard);
     });
 }
 
-
-addButton.addEventListener('click', (e) => {
-    cart.push(e.target.parentElement.parentElement);
+function addToCard(e) {
+    const card = e.target.closest('.dish');
+    const nameSpan = card.querySelector('.dish_name');
+    const dishId = nameSpan.getAttribute('dish_id');
+    cart.push(dishId);
     localStorage.setItem('cart', cart);
-});
-
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://127.0.0.1:8000/dishes/dishes/all')
